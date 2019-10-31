fec7a99c8ef63f43051aaf86d9e7bb5092d021f5
ArroyoBernilla <56905566+ArroyoBernilla@users.noreply.github.com>
2019-10-30 19:28:52 -0500

    Add files via upload

---
+++ chigne.Calculadora.py
@@ -0,0 +1,339 @@
+#calculadora nro1
+#Esta calculadora realiza el calculo del area del triangulo
+
+#Declaracion de variables
+base,altura,area_triangulo=0.0,0.0,0.0
+
+#Calculadora
+base1=20
+altura1=50
+area_triangulo=(base1*altura1)/2
+
+#Mostrar datos
+print("base =", base1)
+print("altura 1=", altura1)
+print(" area del triangulo =", area_triangulo)
+
+
+#Calculadora nro2
+#Esta calculadora realiza el calculo del area del rectangulo
+
+#Declaracion de variables
+base2,altura2,area_rectangulo=0.0,0.0,0.0
+
+#Calculadora
+base2=30
+altura2=100
+area_rectangulo=(base2*altura2)
+
+#Mostrar datos
+print("base =", base2)
+print("altura =", altura2)
+print(" area del rectangulo =", area_rectangulo)
+
+
+#Calculadora nr3
+#Esta calculadora reailza el calculo del area de un cuadrado
+
+#Declaracion de variables
+lado,area_cuadrado=0.0,0.0
+
+#Calculadora
+lado=4
+area_cuadrado=("lado*lado")
+
+#Mostrar datos
+print("lado =", lado)
+print("area del cuadrado=", area_cuadrado)
+
+
+#Calculadora nro4
+#Esta calculadora realiza el calculo del area del circulo
+
+#Declaracion de variables
+pi1,radio1,area_circulo=0.0,0.0,0.0
+
+#Calculadora
+pi1=3.14
+radio1=8
+area_circulo=(pi1*radio1*radio1)
+
+#Mostrar datos
+print("pi=", pi1)
+print("radio =", radio1)
+print("area del circulo=", area_circulo)
+
+
+#calculadora nro5
+#Esta calculadora realiza el calculo del area de la esfera
+
+#Declaracion de variables
+pi2,radio2,area_esfera=0.0,0.0,0.0
+
+#calculadora
+pi2=3.14
+radio2=15
+area_esfera=(4*pi2*radio2*radio2)
+
+#Mostrar datos
+print("pi2=", pi2)
+print("radio2=",radio2)
+print("area de la esfera=",area_esfera)
+
+
+#calculadora nro6
+#Esta calculadora realiza el calculo del area del cubo
+
+#Declaracion de las variables
+arista1,area_cubo=0.0,0.0
+
+#Calculadora
+arista1=5
+area_cubo=(6*arista1*arista1)
+
+#Mostrar datos
+print("arista1=",arista1)
+print("area del cubo=", area_cubo)
+
+
+#calculadora nro7
+#Esta calculadora realiza el calculo del volumen del cubo
+
+#Declaracion de variables
+arista2,volumen_cubo=0.0,0.0
+
+#calculadora
+arista2=36
+volumen_cubo=(arista2*arista2*arista2)
+
+#Mostrar datos
+print("arista2=",arista2)
+print("volumen del cubo=",volumen_cubo)
+
+
+#calculadora nro8
+#Esta calculadora realiza el calculo del volumen de la esfera
+
+#Declaracion de las variables
+pi3,radio3,volumen_esfera=0.0,0.0,0.0
+
+#Calculadora
+pi3=3.14
+radio3=80
+volumen_esfera=(4/3*pi3*radio3*radio3*radio3)
+
+#Mostrar datos
+print("pi3=",pi3)
+print("radio3=",radio3)
+print("volumen de la esfera=",volumen_esfera)
+
+
+#Calculadora nro9
+#Esta calculadora realiza el area del paralelepepedo
+
+#Declaracion de datos
+base_mayos,base_menos,altura3,area_paralelepipedo=0,0,0,0
+
+#calculadora
+area_mayor=13
+area_menor=5
+altura3=10
+area_paralelepipedo=(area_mayor*area_menor+altura3)
+
+#mostrar datos
+print("area mayos=",area_mayor)
+print("area menor=",area_menor)
+print("altura=",altura3)
+print("area del paralelepipedo=",area_paralelepipedo)
+
+
+#calculadora nro10
+#Esta calculadora realiza el calculo de la densidad
+
+#Declaracion de datos
+masa,volumen,densidad=0.0,0.0,0.0
+
+#calculadora
+masa1=5000
+volumen1=1000
+densidad=(masa1/volumen1)
+
+#mostrar datos
+print("masa=",masa1)
+print("volumen=",volumen1)
+print("densidad del cuuerpo=",densidad)
+
+
+#calculadora nro11
+#Esta calculadora realiza el calculo del area del trapecio
+
+#Declaracion de datos
+base_mayor2,base_menor2,altura4,area_trapecio=0.0,0.0,0.0,0.0
+
+#calculadora
+base_mayor2=20
+base_menor2=10
+altura4=5
+area_trapecio=(base_mayor2+base_menor2*altura4)/2
+
+#mostrar datos
+print("base mayor2=",base_mayor2)
+print("base menor2=",base_menor2)
+print("altura del trapecio=",altura4)
+print("area del trapecio=",area_trapecio)
+
+#calculadora nro12
+#Esta calculadora realiza el calculo del costo total
+
+#Declaracion de datos
+costo_fijo,costo_variable,costo_total=0.0,0.0,0.0
+
+#calculadora
+costo_fijo=1000
+costo_variable=2000
+costo_total=(costo_fijo+costo_variable)
+
+#mostrar datos
+print("costo fijo=",costo_fijo)
+print("costo variable=",costo_variable)
+print("costo total=",costo_total)
+
+
+#calculadora nro13
+#Esta calculadora realiza el calculo de la producion
+
+#Declaracion de datos
+costo_fijo2,costo_fijo_medio,producion=0.0,0.0,0.0
+
+#calculadora
+costo_fijo2=2000
+costo_fijo_medio=100
+producion=(costo_fijo2/costo_fijo_medio)
+
+#mostrar datos
+print("costo fijo2=",costo_fijo2)
+print("costo fijo medio=",costo_fijo_medio)
+print("producion =",producion)
+
+
+#calculador nro14
+#Esta calculadora realiza el calculo del costo medio
+
+#Declaracion de datos
+costo_total2,producion2,costo_medio=0.0,0.0,0.0
+
+#calculadora
+costo_total2=1500
+producion2=50
+costo_medio=(costo_total2/producion2)
+
+#mostar datos
+print("costo total 2=",costo_total2)
+print("producion 2=",producion2)
+print("costo medio=", costo_medio)
+
+
+#calculadora nro15
+#Esta calculadora realiza el calculo del ingreso total
+
+#Declaracion de datos
+cantidad_vendida,precio_venta,ingreso_total=0,0.0,0.0
+
+#calculadora
+cantidad_vendida=80
+precio_venta=30
+ingreso_total=(cantidad_vendida*precio_venta)
+
+#Mostrar datos
+print("cantidad vendida=",cantidad_vendida)
+print("precio de venta=",precio_venta)
+print("ingreso total=",ingreso_total)
+
+
+#calculadora nro16
+#Esta calculadora realiza el calcula de la utilidad
+
+#Declaracion de datos
+ingreso_total2,costo_total3,utilidad=0.0,0.0,0.0
+
+#calculadora
+ingreso_total2=3000
+costo_total3=1500
+utilidad=(utilidad-costo_total3)
+
+#mostrar datos
+print("ingreso total 2=",ingreso_total2)
+print("costo total 3=",costo_total3)
+print("utilidad=",utilidad)
+
+
+#calculadora nro17
+#Esta calculadora realiza el calculo del area del cono
+
+#Declaracion de datos
+pi4,radio4,generatris,area_cono=0.0,0.0,0.0,0.0
+
+#calculadora
+pi4=3.14
+radio4=7
+generatris=10
+area_cono=(pi4*radio4*generatris)
+
+#Mostrar datos
+print("pi 4=",pi4)
+print("radio 4=",radio4)
+print("generatris=",generatris)
+print("area del cono=",area_cono)
+
+#calculadora nro18
+#Esta calculadora realiza el calculo del area del cilindro
+
+#Declaracion de datos
+pi5,radio5,generatris2,area_cilindro=0.0,0.0,0.0,0.0
+
+#calculadora
+pi5=3.14
+radio5=10
+generatris2=20
+area_cilindro=(2*pi5*radio5*generatris2)
+
+#mostrar datos}
+print("pi5=",pi5)
+print("radio 5=",radio5)
+print("generatris 2=",generatris2)
+print("area del cilindro=",area_cilindro)
+
+
+#calculadora nro19
+#Esta calculadora realiza el calculo de la distancia
+
+#Declaracion de datos
+velocidad,tiempo,distancia=0.0,0.0,0.0
+
+#calculadora
+velocidad=50
+tiempo=10
+distancia=(velocidad*tiempo)
+
+#Mostrar datos
+print("velocidad del movil=",velocidad)
+print("tiempo=",tiempo)
+print("distancia recorrida=",distancia)
+
+
+#calculadora nro20
+#Esta calculadora realiza el calculo del peso
+
+#Declaracion de datos
+masa3,gravedad,peso=0.0,0.0,0.0
+
+#calculadora
+masa3=500
+gravedad=10
+peso=(masa3/gravedad)
+
+#Mostrar datos
+print("masa 3=",masa3)
+print("garvedad =",gravedad)
+print("peso del cuerpo=",peso)
+
